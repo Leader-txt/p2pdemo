e958cf2f4d11f60da73d42d8f32aeece0c6abd96
Leader <3251263056@qq.com>
2022-11-20 23:35:31 +0800

    upload files

---
+++ p2pdemo-client/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace p2pdemo_client
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var client = new UdpClient();
+            var data = Encoding.UTF8.GetBytes("me");
+            client.Send(data, data.Length, new IPEndPoint(IPAddress.Parse("49.232.218.188"), 333));
+
+            var server = new IPEndPoint(IPAddress.Any, 0);
+            var who = Encoding.UTF8.GetString(client.Receive(ref server));
+            Console.WriteLine("who:"+who);
+            var oth = new IPEndPoint(IPAddress.Parse(who.Split(':')[0]), int.Parse(who.Split(':')[1]));
+            data = Encoding.UTF8.GetBytes("hello I'm client" + who.Split(':')[2]);
+            client.Send(data, data.Length, oth);
+            while (true)
+            {
+                var temp = new IPEndPoint(IPAddress.Any, 0);
+                Console.WriteLine(who.Split(':')[2]+Encoding.UTF8.GetString(client.Receive(ref temp)));
+                data = Encoding.UTF8.GetBytes(Guid.NewGuid() + "from client#" + who.Split(':')[2]+temp);
+                client.Send(data,data.Length, oth);
+            }
+        }
+    }
+}
---
+++ p2pdemo/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace p2pdemo
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var server = new UdpClient(new IPEndPoint(IPAddress.Any,333));
+            var clients = new IPEndPoint[2];
+            while (true)
+            {
+                if(clients[0] == null)
+                {
+                    server.Receive(ref clients[0]);
+                    Console.WriteLine($"client0:{clients[0]}");
+                }
+                else
+                {
+                    server.Receive(ref clients[1]);
+                    Console.WriteLine($"client1:{clients[1]}");
+                    var data0 = Encoding.UTF8.GetBytes(clients[1].ToString()+":0");
+                    var data1 = Encoding.UTF8.GetBytes(clients[0].ToString()+":1");
+                    server.Send(data0, data0.Length, clients[0]);
+                    server.Send(data1, data1.Length, clients[1]);
+                    clients[0]=clients[1]=null;
+                }
+            }
+        }
+    }
+}
